9fbf9b7d8ad73f11fa8c0160eb54235bf9e6224e
joongsoopark <wndtn853@gmail.com>
2020-11-14 17:20:14 +0900

    [#28] Add swagger document

--- src/main/java/com/depromeet/dodo/auth/controller/AuthorizeController.java
+++ src/main/java/com/depromeet/dodo/auth/controller/AuthorizeController.java
@@ -2,6 +2,7 @@ package com.depromeet.dodo.auth.controller;
 
 import com.depromeet.dodo.auth.common.UserInfo;
 import com.depromeet.dodo.auth.thirdparty.naver.NaverAuthService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ public class AuthorizeController {
 	private final NaverAuthService naverAuthService;
 
 	@GetMapping("/third-parties/naver/users")
+	@ApiOperation("네이버 유저의 정보를 가져오는 API")
 	public UserInfo getKakaoUserInfo(@RequestParam String token) {
 
 		return naverAuthService.getUserInfo(null, token);
