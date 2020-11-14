f99b6b19c004b91ea7aa340ebac0e67dd48ccadd
joongsoopark <wndtn853@gmail.com>
2020-11-14 15:06:24 +0900

    [#24] Apply swagger

--- src/main/java/com/depromeet/dodo/config/SwaggerConfig.java
+++ src/main/java/com/depromeet/dodo/config/SwaggerConfig.java
@@ -3,13 +3,10 @@ package com.depromeet.dodo.config;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.Optional;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
@@ -20,7 +17,6 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
-				.build()
-				.genericModelSubstitutes(Optional.class, Flux.class, Mono.class);
+				.build();
 	}
 }
