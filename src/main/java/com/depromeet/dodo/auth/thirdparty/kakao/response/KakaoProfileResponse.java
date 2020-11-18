d532b7777fa16b85a355ae3dce683eb1332bf58e
hyerin6 <hyerinn6@gmail.com>
2020-11-18 23:03:59 +0900

    feat: 카카오 회원가입 기능

--- src/main/java/com/depromeet/dodo/auth/thirdparty/kakao/response/KakaoProfileResponse.java
+++ src/main/java/com/depromeet/dodo/auth/thirdparty/kakao/response/KakaoProfileResponse.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.sun.istack.NotNull;
 
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class KakaoProfileResponse {
 
+	@NotNull
 	private Long id;
+
 	private KakaoAccount kakaoAccount;
 
 	@JsonCreator
