2ca955b4e25691aa69fb5f440571696e32d0d046
hyerin6 <hyerinn6@gmail.com>
2020-12-02 00:19:03 +0900

    [#35] Reflect review

--- src/test/java/com/depromeet/dodo/ApplicationTests.java
+++ src/test/java/com/depromeet/dodo/ApplicationTests.java
@@ -1,9 +1,7 @@
 package com.depromeet.dodo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApplicationTests {
 
 	@Test
